eea2c94c9f531fb0b0224a5f51e346db9aab3a1b
Ricardo Chavez <rchavez@devosinc.com>
2020-12-08 23:54:36 -0500

    Day 7 part 2 / Día 7 parte 2

--- day7.ts
+++ day7.ts
@@ -38,13 +38,7 @@ function findBagsContaining(bagColor: string, rules: Map<string, BagContent[]>):
     return bagColors;
 }
 
-import { createInterface } from "readline";
-
-const rl = createInterface({input: process.stdin});
-
-let rules = new Map<string, BagContent[]>();
-
-rl.on("line", line => parseLine(line, rules)).on("close", () => {
+function part1(rules: Map<string, BagContent[]>) {
     let inputBagColors = ['shiny gold'];
     let outputBagColors = new Set<string>();
     let firstColor: string;
@@ -60,5 +54,32 @@ rl.on("line", line => parseLine(line, rules)).on("close", () => {
             outputBagColors.add(newColor);
         })
     }
-    console.log("final colors:", outputBagColors);
-});
+    console.log("final colors:", outputBagColors.size);
+}
+
+function recursiveContentCount(count: number, bagColor: string, rules: Map<string, BagContent[]>): number {
+    let bagContents = rules.get(bagColor);
+    if (bagContents) {
+        let totalContents = bagContents.reduce((acum, curr) => {
+            return acum + curr.bagCount + recursiveContentCount(curr.bagCount, curr.bagColor, rules);
+        }, 0);
+        return totalContents * count;
+    }
+    // else
+    return 0;
+}
+
+function part2(rules: Map<string, BagContent[]>) {
+    let startingBagColor = "shiny gold";
+
+    let grandTotal = recursiveContentCount(1, startingBagColor, rules);
+    console.log("Grand total:", grandTotal);
+}
+
+import { createInterface } from "readline";
+
+const rl = createInterface({input: process.stdin});
+
+let rules = new Map<string, BagContent[]>();
+
+rl.on("line", line => parseLine(line, rules)).on("close", () => part2(rules));
