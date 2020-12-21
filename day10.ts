e77d586807a8c8f8202584b0f38013e5aefd7ba1
Ricardo Chavez <rchavez@devosinc.com>
2020-12-21 00:31:36 -0500

    Day 10 part 2 / Día 10 parte 2
    Gracias a la comunidad por enseñarme lo que es la secuencia de
    Tribonacci :D

--- day10.ts
+++ day10.ts
@@ -4,9 +4,7 @@ const rl = createInterface({input: process.stdin});
 
 let joltageRatings: number[] = [];
 
-rl.on("line", line => joltageRatings.push(parseInt(line))).on("close", () => {
-    joltageRatings.sort((a, b) => a - b);
-    joltageRatings.unshift(0) // charging outlet
+function part1(joltageRatings: number[]) {
     let differenceCount: [number, number, number] = [0, 0, 0];
     for (let i = 1; i < joltageRatings.length; i++) {
         switch (joltageRatings[i] - joltageRatings[i-1]) {
@@ -28,4 +26,30 @@ rl.on("line", line => joltageRatings.push(parseInt(line))).on("close", () => {
     differenceCount[2] += 1;
     console.log("Differences:", differenceCount);
     console.log("Result:", differenceCount[0] * differenceCount[2]);
+}
+
+function part2(joltageRatings: number[]) {
+    let tribonacciSequence = [1,1,2,4,7,13,24,44,81,149];
+    let combinations = 1;
+    let countOfSuccesiveOnes = 0;
+    for (let i = 1; i < joltageRatings.length; i++) {
+        if (joltageRatings[i] - joltageRatings[i-1] == 1) {
+            countOfSuccesiveOnes += 1;
+        } else {
+            combinations *= tribonacciSequence[countOfSuccesiveOnes];
+            countOfSuccesiveOnes = 0;
+        }
+    }
+    if (countOfSuccesiveOnes > 0) {
+        combinations *= tribonacciSequence[countOfSuccesiveOnes];
+        countOfSuccesiveOnes = 0;
+    }
+    console.log("Combinations:", combinations);
+}
+
+rl.on("line", line => joltageRatings.push(parseInt(line))).on("close", () => {
+    joltageRatings.sort((a, b) => a - b);
+    joltageRatings.unshift(0) // charging outlet
+    part1(joltageRatings);
+    part2(joltageRatings);
 });
