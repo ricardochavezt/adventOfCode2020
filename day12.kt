07b85f40dc421ba8f42aedf1656d736c366aa627
Ricardo Chavez <rchavez@devosinc.com>
2020-12-27 02:52:56 -0500

    Day 12 part 2 / Día 12 parte 2

--- day12.kt
+++ day12.kt
@@ -6,17 +6,35 @@ enum class Direction {
     NORTH, EAST, SOUTH, WEST
 }
 
-class Ship(var x: Int, var y: Int, var direction: Direction) {
-    fun move(distance: Int, direction: Direction) {
-        when (direction) {
-            Direction.NORTH -> y = y + distance
-            Direction.SOUTH -> y = y - distance
-            Direction.EAST -> x = x + distance
-            Direction.WEST -> x = x - distance
+data class Point(val x: Int, val y: Int) {
+    fun move(distance: Int, direction: Direction): Point {
+        return when (direction) {
+            Direction.NORTH -> this.copy(y = y + distance)
+            Direction.SOUTH -> this.copy(y = y - distance)
+            Direction.EAST -> this.copy(x = x + distance)
+            Direction.WEST -> this.copy(x = x - distance)
         }
     }
 
+    fun rotateLeftAroundOrigin() = Point(x=-y, y=x)
+
+    fun rotateRightAroundOrigin() = Point(x=y, y=-x)
+}
+
+class Ship(var position: Point, var direction: Direction, var waypoint: Point = Point(0,0)) {
+    fun move(distance: Int, direction: Direction) {
+        position = position.move(distance, direction)
+    }
+
     fun move(distance: Int) = move(distance, this.direction)
+
+    fun moveWaypoint(distance: Int, direction: Direction) {
+        waypoint = waypoint.move(distance, direction)
+    }
+
+    fun moveTowardsWaypoint(factor: Int) {
+        position = Point(x=position.x+waypoint.x*factor, y=position.y+waypoint.y*factor)
+    }
 
     fun rotateLeft() {
         when (this.direction) {
@@ -35,19 +53,17 @@ class Ship(var x: Int, var y: Int, var direction: Direction) {
             Direction.WEST -> direction = Direction.NORTH
         }
     }
+
+    fun rotateWaypointLeft() {
+        waypoint = waypoint.rotateLeftAroundOrigin()
+    }
+
+    fun rotateWaypointRight() {
+        waypoint = waypoint.rotateRightAroundOrigin()
+    }
 }
 
-fun main() {
-    val lineRegex = Regex("([A-Z])(\\d+)", RegexOption.IGNORE_CASE)
-    val instructions = emptyList<Instruction>().toMutableList()
-    val ship = Ship(x = 0, y = 0, direction = Direction.EAST)
-    do {
-        val line = readLine()
-        lineRegex.matchEntire(line ?: "")?.let {
-            val (action, value) = it.destructured
-            instructions.add(Instruction(action, value.toInt()))
-        }
-    } while (line != null)
+fun part1(instructions: List<Instruction>, ship: Ship) {
     for (instruction in instructions) {
         when (instruction.action) {
             "N" -> ship.move(instruction.value, Direction.NORTH)
@@ -63,5 +79,40 @@ fun main() {
             }
         }
     }
-    println("Final Manhattan distance: ${abs(ship.x) + abs(ship.y)}")
+    println("Part 1: Final Manhattan distance: ${abs(ship.position.x) + abs(ship.position.y)}")
+}
+
+fun part2(instructions: List<Instruction>, ship: Ship) {
+    for (instruction in instructions) {
+        when (instruction.action) {
+            "N" -> ship.moveWaypoint(instruction.value, Direction.NORTH)
+            "S" -> ship.moveWaypoint(instruction.value, Direction.SOUTH)
+            "E" -> ship.moveWaypoint(instruction.value, Direction.EAST)
+            "W" -> ship.moveWaypoint(instruction.value, Direction.WEST)
+            "F" -> ship.moveTowardsWaypoint(instruction.value)
+            "L" -> {
+                for (i in 1..(instruction.value / 90)) ship.rotateWaypointLeft()
+            }
+            "R" -> {
+                for (i in 1..(instruction.value / 90)) ship.rotateWaypointRight()
+            }
+        }
+    }
+    println("Part 2: Final Manhattan distance: ${abs(ship.position.x) + abs(ship.position.y)}")
+}
+
+fun main() {
+    val lineRegex = Regex("([A-Z])(\\d+)", RegexOption.IGNORE_CASE)
+    val instructions = emptyList<Instruction>().toMutableList()
+    do {
+        val line = readLine()
+        lineRegex.matchEntire(line ?: "")?.let {
+            val (action, value) = it.destructured
+            instructions.add(Instruction(action, value.toInt()))
+        }
+    } while (line != null)
+
+    part1(instructions=instructions, ship=Ship(position=Point(x=0, y=0), direction=Direction.EAST))
+
+    part2(instructions=instructions, ship=Ship(position=Point(x=0, y=0), direction=Direction.EAST, waypoint=Point(x=10, y=1)))
 }
